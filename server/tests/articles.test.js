25b709e0d52ab6063eddbe694ee7984705735795
hirwaf <hirwa.felix@gmail.com>
2019-09-27 18:07:14 +0200

    feature(article-api):build edit article API endpoint
    - build API endpoint `/api/v1/articles/:articleId`
    - add test for `/api/v1/articles/:articleId` api
    - add rules(`no-prototype-builtins Off`) in .eslintrc.json
    - add test for Auth
    - install new packages `sinon`, `sinon-chai`, `sinon-express-mock`
    
    [ Finishes #168380741 ]

--- server/tests/articles.test.js
+++ server/tests/articles.test.js
@@ -190,4 +190,64 @@ describe('Articles endpoint tests', () => {
       });
     done();
   });
+
+  it('should fail to edit article due to unavailability of articleId', (done) => {
+    const articleId = 8;
+    const data = {
+      title: 'Eget duis at tellus at urna condimentum mattis pellentesque id',
+      image: 'https://images.unsplash.com/photo-1568685002001-1017b6b99e44?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=610&q=80',
+      article: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    };
+    chai.request(server)
+      .patch(`/api/v1/articles/${articleId}`)
+      .set('token', token)
+      .send(data)
+      .end((request, response) => {
+        response.body.should.have.property('status')
+          .equal(404);
+        response.body.should.have.property('message').equal('Article no found !');
+      });
+    done();
+  });
+
+  it('should fail to edit article due to validation', (done) => {
+    const articleId = 4;
+    chai.request(server)
+      .patch(`/api/v1/articles/${articleId}`)
+      .set('token', token)
+      .send({})
+      .end((request, response) => {
+        response.body.should.have.property('status')
+          .equal(422);
+      });
+    done();
+  });
+
+  it('should edit article', (done) => {
+    const articleId = 4;
+    const data = {
+      title: 'Eget duis at tellus at urna condimentum mattis pellentesque id',
+      image: 'https://images.unsplash.com/photo-1568685002001-1017b6b99e44?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=610&q=80',
+      article: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    };
+    chai.request(server)
+      .patch(`/api/v1/articles/${articleId}`)
+      .set('token', token)
+      .send(data)
+      .end((request, response) => {
+        response.body.should.have.property('status')
+          .equal(200);
+        response.body.should.have.property('message')
+          .equal('Article successfully edited');
+        response.body.should.have.property('data');
+        response.body.data.should.be.an('Object');
+        response.body.data.should.have.property('title')
+          .equal(data.title);
+        response.body.data.should.have.property('image')
+          .equal(data.image);
+        response.body.data.should.have.property('article')
+          .equal(data.article);
+      });
+    done();
+  });
 });
