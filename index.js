2c754f37e095c4d6a251bcff10d0aee8dc1083fe
hirwaf <hirwa.felix@gmail.com>
2019-10-02 13:49:28 +0200

    ch(documentation):document API endpoints
    - install swagger-express
    - touch swagger.json
    - add swagger in index.js
    - add documentation routes
    
    [ Finishes #168825684 ]

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
+import swagger from 'swagger-ui-express';
 import routers from './server/routers';
+import swaggerJson from './swagger';
 
 dotenv.config();
 const app = express();
@@ -14,7 +16,7 @@ app.use(cors());
 app.use(morgan('short'));
 
 app.use('/api/v1/', routers);
-
+app.use('/docs/v1/', swagger.serve, swagger.setup(swaggerJson));
 app.listen(PORT, () => {
   console.log(`Magic happens on port ${PORT}`);
 });
