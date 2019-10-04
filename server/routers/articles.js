4ee861c907e076dc93c892b6a42c04acbee66445
hirwaf <hirwa.felix@gmail.com>
2019-10-04 11:40:13 +0200

    feature(articles):view articles by author
    - create `findByAuthor` method inside `ArticlesController`
    - having `/api/v1/articles/:authorId/author` API endpoint tests
    - having `/api/v1/articles/:authorId/author` API endpoint working
    
    [ Finishes #168947186 ]

--- server/routers/articles.js
+++ server/routers/articles.js
@@ -15,5 +15,5 @@ articles.route('/articles/:articleId').delete(ArticlesController.destroy);
 articles.route('/articles/:articleId/comments').post(ArticlesController.addComment);
 articles.route('/articles/:articleId').patch(ArticlesController.update);
 articles.route('/feeds/:tagId/tags').get(ArticlesController.findByTag);
-
+articles.route('/author/articles/:authorId').get(ArticlesController.findByAuthor);
 export default articles;
