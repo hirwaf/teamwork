a56561a640e82e202ff7c643088efba97323d9cd
hirwaf <hirwa.felix@gmail.com>
2019-10-10 13:51:58 +0200

    feature(database):add comment
    - Add create_comments_table migration
    - add to init_db
    - add Comment Model
    - upgrade `addComment` method in `ArticleController`
    - update update related tests
    
    [ Finishes #168979137 ]

--- server/database/init_db.js
+++ server/database/init_db.js
@@ -1,8 +1,10 @@
 import { CreateEmployeesTable, CreateArticlesTable } from './migrations';
+import CreateCommentsTable from './migrations/create_comments_table';
 
 const InitDB = async () => {
   await CreateEmployeesTable.run();
   await CreateArticlesTable.run();
+  await CreateCommentsTable.run();
 };
 
 module.exports = InitDB;
